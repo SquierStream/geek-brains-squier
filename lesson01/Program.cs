7091441383244b4948b03e866ebafed97a4cce54
e.moiseev <e.moiseev@gaijin.team>
2022-08-09 21:40:17 +0300

    lesson01 done

---
+++ lesson01/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.WriteLine("Напишите первое число");
+string? firstLine = Console.ReadLine();
+int firstValue = Convert.ToInt16(firstLine);
+
+Console.WriteLine("Напишите второе число");
+string? secondLine = Console.ReadLine();
+int secondValue = Convert.ToInt16(secondLine);
+
+if (firstValue > secondValue) {
+    Console.WriteLine("{0} > {1}", firstValue, secondValue);  // строковая интерполяция
+} else if (firstValue < secondValue) {
+    Console.WriteLine("{0} < {1}", firstValue, secondValue);
+} else {
+    Console.WriteLine("Кажется, эти числа равны!");
+}
