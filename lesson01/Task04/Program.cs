8a30354f2f609fcd9a0becb82b30d2bc5e47c427
e.moiseev <e.moiseev@gaijin.team>
2022-08-11 21:11:10 +0300

    for test

---
+++ lesson01/Task04/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Напишите первое число");
+string? firstLine = Console.ReadLine();
+int firstValue = Convert.ToInt16(firstLine);
+
+Console.WriteLine("Напишите второе число");
+string? secondLine = Console.ReadLine();
+int secondValue = Convert.ToInt16(secondLine);
+
+Console.WriteLine("Напишите третье число");
+string? thirdLine = Console.ReadLine();
+int thirdValue = Convert.ToInt16(thirdLine);
+
+int a = firstValue;
+int b = secondValue;
+int c = thirdValue;
+int max = Math.Max(a, b);
+max = Math.Max(max, c);
+Console.WriteLine("Максимальное число " + max);
+Console.ReadLine();
