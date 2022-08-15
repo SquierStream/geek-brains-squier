cdc8f139812421ed1a81ea2aa94c1d08a306d128
e.moiseev <e.moiseev@gaijin.team>
2022-08-15 15:29:07 +0300

    Added Task10

---
+++ lesson02/Task10/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Напишите трёхзначное число");
+int threeDigit = Convert.ToInt16(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigit);
+
+Console.WriteLine("Вторая цифра этого числа "+stringNumber[1]);
