fd01ab0703132d2cf99e3ce3dfe5bdd71ed783fb
e.moiseev <e.moiseev@gaijin.team>
2022-08-15 21:00:18 +0300

    Added seminar task

---
+++ lesson04/Seminar/Program.cs
@@ -0,0 +1,12 @@
+﻿//Написать метод, принимающий бинарное представление числа и возращаюшее десятичное представление числа
+
+Console.Write("Введите бинарное число: ");
+      String Number = Console.ReadLine();
+      Int32 DecimalNumber = 0;
+ 
+      for (int i = 0; i < Number.Length; i++)
+      {
+         if (Number[Number.Length - i - 1] == '0') continue; // Проверка на 0 - останавливаемся 
+         DecimalNumber += (int)Math.Pow(2, i); // Числа возводятся в квадрат и прибавляются в одну переменную
+      }
+      Console.WriteLine("Число в десятичном представлении: " + DecimalNumber);
