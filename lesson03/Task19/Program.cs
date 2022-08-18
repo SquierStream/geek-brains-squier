3e78a196ffa0c7ef51dbd1ca389261ed18f9e18f
e.moiseev <e.moiseev@gaijin.team>
2022-08-18 14:41:09 +0300

    Added Task19

---
+++ lesson03/Task19/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите метод, который принимает на вход шестизначное число и проверяет, является ли оно палиндромом.
+
+Console.WriteLine("Введите шестизначное число: ");
+string? number = Console.ReadLine();
+
+void checkNumber(string number){
+  if (number[0]==number[5] || number[1]==number[4] || number[2]==number[3]){
+    Console.WriteLine($"Указанное число: {number} — палиндром.");
+  }
+  else Console.WriteLine($"Указанное число: {number} — НЕ палиндром.");
+}
+
+if (number!.Length == 6){
+  checkNumber(number);
+}
+else Console.WriteLine($"Проверьте, что ввели шестизначное число!");
