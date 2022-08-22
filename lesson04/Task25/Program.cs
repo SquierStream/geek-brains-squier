b8b60d3a3a5d64ab1c9db73ba6a1766cbc3191e7
e.moiseev <e.moiseev@gaijin.team>
2022-08-22 18:36:54 +0300

    Added Task25

---
+++ lesson04/Task25/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите метод, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.WriteLine("Введите число A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int bSq = b;
+int aSq = a;
+
+while (b > 1){
+    b = b - 1;
+    a = a * aSq;
+}
+
+Console.WriteLine("Ответ:" +a);
