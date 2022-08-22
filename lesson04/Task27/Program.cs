29700ff839140fd26faedcdc79c97636aeb124c2
e.moiseev <e.moiseev@gaijin.team>
2022-08-22 18:47:22 +0300

    Added Task27

---
+++ lesson04/Task27/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите матод, который принимает на вход число и выдаёт сумму цифр в числе.
+Console.WriteLine("Напишите число: ");
+int n = Int32.Parse(Console.ReadLine());
+int sum = 0;
+while (n > 0){
+    sum = sum + n % 10;
+    n /= 10;
+}
+
+Console.WriteLine($"Сумма чисел указанного числа = {sum}");
